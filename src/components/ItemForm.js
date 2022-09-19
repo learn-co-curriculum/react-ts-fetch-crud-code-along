50f5aa6222c00955fd6bf3b751f01aecd4f89a11
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-09-19 12:42:08 -0700

    initialize files

---
+++ src/components/ItemForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function ItemForm() {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  return (
+    <form className="NewItem">
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Category:
+        <select
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </label>
+
+      <button type="submit">Add to List</button>
+    </form>
+  );
+}
+
+export default ItemForm;
